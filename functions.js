6431fd5db8e2477128788eeaab3c351f09ed546c
hsattar <39243545+hsattar@users.noreply.github.com>
2021-09-27 08:59:50 +0100

    Solved Q1

--- functions.js
+++ functions.js
@@ -7,32 +7,64 @@ const printNewLine = question => {
     Write a function called dice; it should randomize an integer number between 1 and 6.
 */
 
+printNewLine(1)
+
+const dice = () => {
+    return Math.ceil(Math.random() * 6)
+}
+
+console.log(dice())
+
 /* EXERCISE 2
     Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
 */
+
+printNewLine(2)
+
+console.log()
 
 /* EXERCISE 3
     Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
     Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
 
+printNewLine(3)
+
+console.log()
+
 /* EXERCISE 4
     Write a function called deleteOne which receives a string and a boolean as parameters.
     If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
 */
+
+printNewLine(4)
+
+console.log()
 
 /* EXERCISE 5
    Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
    Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
 */
 
+printNewLine(5)
+
+console.log()
+
 /* EXERCISE 6
    Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
 */
 
+printNewLine(6)
+
+console.log()
+
 /* EXERCISE 7
    Write a function called whatDayIsIt that should return the current day of the week.
 */
+
+printNewLine(7)
+
+console.log()
 
 /* EXERCISE 8
     Write a function called rollTheDices which receives a number as a parameter.
@@ -45,10 +77,22 @@ const printNewLine = question => {
     }
 */
 
+printNewLine(8)
+
+console.log()
+
 /* EXERCISE 9
    Write a function called howManyDays which receives a date as a parameter and returns the number of days passed since that date.
 */
 
+printNewLine(9)
+
+console.log()
+
 /* EXERCISE 10
    Write a function called isTodayMyBirthday which should return true if today's your birthday, false otherwise.
 */
+
+printNewLine(10)
+
+console.log()
