ef205b9ea5c37951c53ff21e8f1ab9246f933b4f
hsattar <39243545+hsattar@users.noreply.github.com>
2021-09-27 08:52:12 +0100

    Solved Question A - C

--- basics.js
+++ basics.js
@@ -10,29 +10,58 @@ const printNewLine = question => {
 
 printNewLine('A')
 
+let test = 'A Random String'
+console.log(test)
+
 /* EXERCISE B
     Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
 */
+
+printNewLine('B')
+
+let sum = 10 + 20
+console.log(sum)
+
 
 /* EXERCISE C
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 
+printNewLine('C')
+
+let random = Math.floor(Math.random() * 20)
+console.log(random)
+
+
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
+
+printNewLine('D')
 
 /* EXERCISE E
     Write a piece of code for programmatically removing the age property from the previously create object.
 */
 
+printNewLine('E')
+
+
+
 /* EXERCISE F
    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
 */
 
+printNewLine('F')
+
+
+
 /* EXERCISE G
    Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
 */
+
+printNewLine('G')
+
+
 
 
 
