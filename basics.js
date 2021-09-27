bd7ef52e99d88acf6d392444470984fbdcac64ff
hsattar <39243545+hsattar@users.noreply.github.com>
2021-09-27 08:35:18 +0100

    Finished Setup

---
+++ basics.js
@@ -0,0 +1,40 @@
+const printNewLine = question => {
+    console.log(`\n\n------------------ Answer Q${question} ------------------ \n`)
+}
+
+// JS Basics
+
+/* EXERCISE A
+   Create a variable called test and assign a string value to it.
+*/
+
+printNewLine('A')
+
+/* EXERCISE B
+    Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
+*/
+
+/* EXERCISE C
+    Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
+*/
+
+/* EXERCISE D
+    Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
+*/
+
+/* EXERCISE E
+    Write a piece of code for programmatically removing the age property from the previously create object.
+*/
+
+/* EXERCISE F
+   Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
+*/
+
+/* EXERCISE G
+   Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
+*/
+
+
+
+
+
---
+++ functions.js
@@ -0,0 +1,54 @@
+const printNewLine = question => {
+    console.log(`\n\n------------------ Answer Q${question} ------------------ \n`)
+}
+
+// JS Functions
+/* EXERCISE 1
+    Write a function called dice; it should randomize an integer number between 1 and 6.
+*/
+
+/* EXERCISE 2
+    Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
+*/
+
+/* EXERCISE 3
+    Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
+    Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
+*/
+
+/* EXERCISE 4
+    Write a function called deleteOne which receives a string and a boolean as parameters.
+    If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+*/
+
+/* EXERCISE 5
+   Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
+   Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
+*/
+
+/* EXERCISE 6
+   Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
+*/
+
+/* EXERCISE 7
+   Write a function called whatDayIsIt that should return the current day of the week.
+*/
+
+/* EXERCISE 8
+    Write a function called rollTheDices which receives a number as a parameter.
+    It should invoke the dice() function defined in Ex1 the specified amount of times,
+    and return an object containing a sum property holding the sum of all values extracted
+    and a values array containing the single values of the dicerolls themselves.
+    Example: RollTheDices(3) => returns {
+        sum: 10
+        values: [3, 3, 4]
+    }
+*/
+
+/* EXERCISE 9
+   Write a function called howManyDays which receives a date as a parameter and returns the number of days passed since that date.
+*/
+
+/* EXERCISE 10
+   Write a function called isTodayMyBirthday which should return true if today's your birthday, false otherwise.
+*/
